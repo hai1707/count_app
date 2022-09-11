d457569a0cb376935cc32348a4a3b88a4a76a953
hai1707 <thanhhai.tht14@gmail.com>
2022-09-12 02:52:26 +0700

    Fix Logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: avoid_unnecessary_containers
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -15,26 +17,24 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int _count = 1;
-  int _countTwo = 0;
-  int _sum = 0;
-  int _count3 = 0;
+  int _count = 0, _countTwo = 0, _sum = 0, _count3 = 0, _count4 = 0, temp = 0;
+  
   final myController = TextEditingController();
   @override
   void setState(VoidCallback fn) {
     // ignore: todo
     // TODO: implement setState
-    _count;
-    _countTwo;
-    // _count3 = int.parse(_count3Str);
-    _sum = _count+_countTwo + 1;
+    temp = Random().nextInt(10);
+    _sum = (_count + _countTwo + _count3 + _count4) + 1;
     super.setState(fn);
   }
+
   @override
   void dispose() {
     myController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -43,71 +43,106 @@ class _MyAppState extends State<MyApp> {
           crossAxisAlignment: CrossAxisAlignment.end,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
+            FloatingActionButton(
+                backgroundColor: (Colors.blueGrey),
+                onPressed: () {
+                  setState(() {
+                    _count4 = temp;
+                  });
+                },
+                child: const Icon(Icons.question_mark)),
             // Nút thứ 1
-            FloatingActionButton(backgroundColor: (Colors.orange), 
-                onPressed: () {setState(() {_count++;});},
+            FloatingActionButton(
+                backgroundColor: (Colors.orange),
+                onPressed: () {
+                  setState(() {
+                    _count++;
+                  });
+                },
                 child: const Icon(Icons.plus_one)),
             FloatingActionButton(
-                onPressed: (){setState(() {
-                  _sum=0;_countTwo=0; _count=0;_count3 = 0;});},
-                tooltip:"Xóa hết",
-                child: const Icon(Icons.clear),
+              onPressed: () {
+                setState(() {
+                  _sum = 0;
+                  _countTwo = 0;
+                  _count = 0;
+                  _count3 = 0;
+                  _count4 = 0;
+                });
+              },
+              tooltip: "Xóa hết",
+              child: const Icon(Icons.clear),
             ),
             // Nút thứ 3
-            FloatingActionButton(backgroundColor: (Colors.red), 
-                onPressed: () { setState(() {
-                  _countTwo++;
-                }); },
-                child: const Icon(Icons.plus_one)
-              ),  
-            ],
-          ),
-      body: Column(
-        // crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-              width: 50,
-              margin: const EdgeInsets.only(bottom: 80),
-              child: TextField(
-                onChanged: (val){
+            FloatingActionButton(
+                backgroundColor: (Colors.red),
+                onPressed: () {
                   setState(() {
-                  _count3 = int.parse(val);});
+                    _countTwo++;
+                  });
                 },
-                textInputAction: TextInputAction.go,
-                textAlign: TextAlign.center,
-                controller: myController,
-                maxLength: 1,
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.digitsOnly]
-                )  
+                child: const Icon(Icons.plus_one)),
+          ],
+        ),
+        body: Column(
+          // crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                width: 50,
+                margin: const EdgeInsets.only(bottom: 80),
+                child: TextField(
+                    onChanged: (val) {
+                      setState(() {
+                      });
+                        _count3 = int.parse(myController.text);
+                    },
+                    textInputAction: TextInputAction.go,
+                    textAlign: TextAlign.center,
+                    controller: myController,
+                    maxLength: 1,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ])),
+            // const SizedBox(height: 100),
+            Center(
+              child: Wrap(
+                spacing: 50,
+                alignment: WrapAlignment.center,
+                children: [
+                  Container(
+                      // margin: const EdgeInsets.all(15),
+                      child: Text(
+                    "Số thứ tư : $_count4",
+                    style: const TextStyle(color: Colors.blueGrey),
+                  )),
+                  Container(
+                      // margin: const EdgeInsets.all(15),
+                      child: Text(
+                    "Số thứ ba : $_count3",
+                    style: const TextStyle(color: Colors.orange),
+                  )),
+                  Container(
+                      child: Text(
+                    "Số thứ nhất : $_count",
+                    style: const TextStyle(color: Colors.orange),
+                  )),
+                  Container(
+                    child: Text(
+                      "Số thứ hai : $_countTwo",
+                      style: const TextStyle(color: Colors.red),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          // const SizedBox(height: 100),
-          Center(
-            child: Wrap(
-              spacing: 50,
-              alignment: WrapAlignment.center,
-            children: [
-              Container(
-                  // margin: const EdgeInsets.all(15),
-                  child: Text("Số thứ ba : $_count3",style: const TextStyle(color: Colors.orange),)
-              ),
-              Container(
-                  child: Text("Số thứ nhất : $_count",style: const TextStyle(color: Colors.orange),)
-                ),
-              Container(
-                child: Text("Số thứ hai : $_countTwo",style: const TextStyle(color: Colors.red),),
-              ),
-            ],
-          ),
-          ),
-          Container(
-                margin: const EdgeInsets.only(top: 50),
-                child: Text("Tổng 2 số : $_count + $_countTwo = $_sum"),
-              ),
-        ],
-      ),
+            Container(
+              margin: const EdgeInsets.only(top: 50),
+              child: Text("Tổng 4 số : $_count + $_countTwo + $_count3 + $_count4 = $_sum"),
+            ), 
+          ],
+        ),
       ),
     );
   }
